7574186aa7c97883cceed5e4f7ed666d4a819f42
wxzclc <wxzclc@gmail.com>
2023-07-23 12:41:56 +0800

    config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 const isProd = process.env.NODE_ENV === 'production'
-const prefixPath = isProd ? '/wxzclc-web' : ''
+const prefixPath = isProd ? 'https://wxzclc.github.io/wxzclc-web' : undefined
 
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
   swcMinify: true,
   trailingSlash: true,
-  assetPrefix: prefixPath
+  assetPrefix: prefixPath,
+  basePath: isProd ? '/wxzclc-web' : undefined
 }
 
 module.exports = nextConfig
