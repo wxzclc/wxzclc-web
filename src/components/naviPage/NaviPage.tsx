7a0cf67f40bae5dc498041d2f984b5ee91cc99fe
wxzclc <wxzclc@gmail.com>
2023-07-23 13:33:15 +0800

    config

--- src/components/naviPage/NaviPage.tsx
+++ src/components/naviPage/NaviPage.tsx
@@ -1,12 +1,20 @@
 "use client"
 import React, { useState } from 'react';
-import { SearchOutlined, AppstoreOutlined, NodeIndexOutlined, PushpinOutlined } from '@ant-design/icons';
+import { LoadingOutlined } from '@ant-design/icons';
 
 const NaviPage: React.FC = () => {
+  const loadingStyle: React.CSSProperties = {
+    color: 'rgba(255,255,255,0.88)',
+    fontSize: '2em',
+    textAlign: 'center',
+    paddingTop: '35vh',
+  };
+
   return (
     <>
-      <div>asdasd</div>
-      <div>asdd</div>
+      <div style={loadingStyle}>
+        <LoadingOutlined /> 施工中...
+      </div>
     </>
   )
 }
