f3577626ae5d3882500d49acaa07820a5822ad8a
wxzclc <wxzclc@gmail.com>
2023-07-23 11:41:06 +0800

    config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+const prefixPath = isProd ? '/sub-derectory' : ''
+
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
   swcMinify: true,
   trailingSlash: true,
+  basePath: prefixPath,
 }
 
 module.exports = nextConfig
