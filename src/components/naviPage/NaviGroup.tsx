bfc3a92b44ca6db46049637914a5f6b925cf3f5b
wxzclc <wxzclc@gmail.com>
2023-07-23 20:49:23 +0800

    navi page action

--- src/components/naviPage/NaviGroup.tsx
+++ src/components/naviPage/NaviGroup.tsx
@@ -1,19 +1,21 @@
 "use client"
 import React, { useState } from 'react';
-import { GlobalOutlined } from '@ant-design/icons';
+import { LoadingOutlined } from '@ant-design/icons';
 
 const tmpStyle: React.CSSProperties = {
   backgroundColor: 'rgba(230,230,250,0.4)',
   height: 'calc((100vh - 62px - 64px - 32px) / 3)',
   width: '100%',
-  borderRadius: '5px'
+  borderRadius: '5px',
+  textAlign: 'center',
+  lineHeight: '15em'
 };
 
 const NaviGroup: React.FC = () => {
   return (
     <>
       <div style={tmpStyle}>
-        <GlobalOutlined />
+        <LoadingOutlined /> 施工中...
       </div>
     </>
   )
